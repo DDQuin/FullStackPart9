ac0e8f359d8edbd647d5ed47678325f8fff74f60
DDQuin <ddquin420@gmail.com>
2022-07-08 11:11:04 +0100

    ex 9.7

--- ts-ex-b/index.ts
+++ ts-ex-b/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercise } from "./exerciseCalculator";
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, _res) => {
   _res.send("Hello Full Stack!");
@@ -19,6 +21,33 @@ app.get("/bmi", (_req, _res) => {
     bmi: bmiM,
   });
 });
+
+app.post("/exercises", (_req, _res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const {daily_exercises, target} = _req.body;
+  if (!target || !daily_exercises) {
+    return _res.send({ error: 'missing params'}).status(400);
+  }
+  if (isNaN(Number(target))) {
+    return _res.send({ error: 'malformatted paramaters'}).status(400);
+  }
+  if (!Array.isArray(daily_exercises) || !isNumArray(daily_exercises)) {
+    return _res.send({ error: 'malformatted paramaters'}).status(400);
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const ex = calculateExercise(daily_exercises, target);
+  return _res.json(ex);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isNumArray = (numbers: Array<any>): boolean => {
+  for (const num of numbers) {
+    if (isNaN(Number(num))) {
+      return false;
+    }
+    }
+    return true;
+};
 
 const PORT = 3003;
 
