6d718ddb49458d9f44c1526f04074ab36e4160db
DDQuin <ddquin420@gmail.com>
2022-07-10 16:55:17 +0100

    ex 9.17

---
+++ patientor/src/PatientViewPage/index.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import React from "react";
+import axios from "axios";
+import { Gender, Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import Fireplace from '@material-ui/icons/Fireplace';
+import Flag from '@material-ui/icons/Flag';
+
+const PatientViewPage = () => {
+    const { id } = useParams<{ id: string }>();
+    const [{ patientPage }, dispatch] = useStateValue();
+    
+    const fetchPatient = async () => {
+        try {
+          const { data: patientFromApi } = await axios.get<Patient>(
+            `${apiBaseUrl}/patients/${id}`
+          );
+          dispatch({ type: "SET_PATIENT", payload: patientFromApi });
+        } catch (e) {
+          console.error(e);
+        }
+      };
+
+    if (!patientPage || patientPage.id != id) {
+        void fetchPatient();
+    }
+
+    return (
+        <div>
+            <h2>{patientPage?.name} {patientPage?.gender == Gender.Male ? <Flag/> : <Fireplace/>}</h2>
+
+            <br>
+            </br>
+            ssn: {patientPage?.ssn}<br>
+            </br>
+            occupation: {patientPage?.occupation}
+             
+        </div>
+    );
+    
+
+};
+
+export default PatientViewPage;
