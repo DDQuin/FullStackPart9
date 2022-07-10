3ce5a99002643d43b81735c49ca839579f165799
DDQuin <ddquin420@gmail.com>
2022-07-10 17:30:32 +0100

    ex 9.19

--- patient-backend/src/types.ts
+++ patient-backend/src/types.ts
@@ -10,9 +10,7 @@ export interface Diagnose {
     Female = 'female',
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
+
 
   export interface Patient {
     id: string;
@@ -29,5 +27,47 @@ export interface Entry {
   export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
 
   export type NewPatient = Omit<Patient, 'id'>;
+
+  interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: string[];
+  }
+
+  export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+  
+  interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+  }
+
+  interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: {
+      date: string,
+      criteria: string,
+    },
+  }
+
+  interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string,
+    sickLeave: {
+      startDate: string,
+      endDate: string
+    }
+  }
+
+  export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
  
